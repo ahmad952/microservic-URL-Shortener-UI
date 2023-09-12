3f067278cc5db4bf7f99327e2f911b8962835026
Ahmad abou altout <69421112+ahmad952@users.noreply.github.com>
2023-09-12 02:42:27 +0200

    Update i18n.ts

--- src/lokalisierung/i18n.ts
+++ src/lokalisierung/i18n.ts
@@ -2,7 +2,6 @@ import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 
-// Ihre Übersetzungsressourcen
 const resources = {
   en: {
     translation: {
